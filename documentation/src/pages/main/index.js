6eb876cc2172135537c340d6b27bb7d44b627252
Antoni André <antoniandre.web@gmail.com>
2018-07-24 21:31:45 +0800

    externalize CSS and cleanup.

--- documentation/src/pages/main/index.js
+++ documentation/src/pages/main/index.js
@@ -1,16 +1,11 @@
 import { simpleSyntaxHighlighter } from '../../../../dist/simple-syntax-highlighter'
 import { highlight } from '../../components/highlight-message'
 
+import '../../../../dist/simple-syntax-highlighter.min.css'
 import './style.scss'
 import './examples.scss'
 
 export default {
   components: { sshPre: simpleSyntaxHighlighter, highlight },
-  template: require('./template.pug'),
-  data: () => ({
-  }),
-  methods: {
-  },
-  created () {
-  }
+  template: require('./template.pug')
 }
