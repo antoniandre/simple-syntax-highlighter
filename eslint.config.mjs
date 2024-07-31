1d432600b158ce6192073aec32d4f296f5a500a9
antoniandre <antoniandre.web@gmail.com>
2024-07-31 22:58:21 +0200

    Update packages and add eslint.

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import importPlugin from 'eslint-plugin-import'
+import nPlugin from 'eslint-plugin-n'
+import promisePlugin from 'eslint-plugin-promise'
+import standard from 'eslint-config-standard'
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,vue}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  {
+    name: 'standard',
+    rules: standard.rules,
+    plugins: {
+      import: importPlugin,
+      n: nPlugin,
+      promise: promisePlugin
+    }
+  },
+  ...pluginVue.configs['flat/essential']
+]
